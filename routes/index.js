da645f8c189292f93601c34ce08b4bc39b9f93ce
Aziz Amerul Faozi <admin@labseni.com>
2018-11-08 13:09:37 +0700

    assek

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Kalista Wacan' });
 });
 
+router.get('/tentang', function(req, res, next){
+  res.render('index', {title: 'Tentang'});
+}
+);
 module.exports = router;
